5cc8f179d9f2960e5aba93cf0fa409eaedd4ba8e
Alex Recker <alex@reckerfamily.com>
2015-05-12 23:09:39 -0500

    Figured out fab env and config

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,2 +1,43 @@
+from fabric.api import *
+
+
+# Get Rood Directory
 import os
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+
+# Get Raw Config Data
+import json
+CONFIG = None
+with open(os.path.join(PROJECT_ROOT, '..', 'config.json')) as file:
+    CONFIG = json.load(file)
+
+
+# Setup fab environment
+for item in CONFIG['env']:
+    setattr(env, item, CONFIG['env'][item])
+
+
+class InputModel(object):
+    """
+    Everything we'll need from the user
+    Gets handed back and forth between tasks
+    """
+    def __init__(self):
+        pass
+
+
+@task
+def handshake(model=None):
+    """
+    Say hello to the server
+    """
+    run('hostname')
+    print('Handshake successful')
+
+
+if __name__ == '__main__':
+    import sys
+    from fabric.main import main
+    sys.argv = ['fab', '-f', __file__, 'handshake']
+    main()
